9f0783ddf86c3c04278764fe1df58cc4b8dc8230
Yu Liu <44285873+liu-psy@users.noreply.github.com>
2020-08-03 12:02:29 +0800

    Upload data and scripts
    ROI.txt series comes from  t-test of subtypes (SPM-matlab)

---
+++ ROI/File.R
@@ -0,0 +1,78 @@
+# This script was used to extract file path and covariates for SPM analysis
+library(xlsx)
+library(dplyr)
+
+
+setwd("E:/WorkingSpace/R/Paper/Depression/Subtypes/ROI")
+# Load data --------------------------------------------------------------------
+home <- "E:/WorkingSpace/R/Paper/Depression/Subtypes/HHC/HHC.xlsx"
+Patient <- read.xlsx2(home, 1, stringsAsFactors = FALSE,
+  colClasses = rep(c("character", "numeric"), times = c(8, 29)), check.names = FALSE)
+
+Patient$Sites <- factor(Patient$Sites, levels = 1:15)
+rm(home)
+
+# Check subgroups --------------------------------------------------------------
+# 1 for LOI;2 for SAI
+with(Patient, ftable(Level1))
+# 1vs4  2vs3. 1 for LOI+; 4 for LOI-; 3 for SAI+; 4 for SAI-
+with(Patient, ftable(Level1, Level2))   
+
+# Function factory -------------------------------------------------------------
+extract <- function(vars) {
+  extract_vars <- function(level, cluster) {
+  dirs <- Patient[Patient[[level]] %in% cluster, ][vars]
+  dirs
+  }
+}
+
+# Extract SPM directory for every patient --------------------------------------
+extract_dir <- extract("Dir")
+Dirs <- list("1-1" = extract_dir("Level1", 1), "1-2" = extract_dir("Level1", 2),
+  "2-1" = extract_dir("Level2", 1), "2-4" = extract_dir("Level2", 4),
+  "2-2" = extract_dir("Level2", 2), "2-3" = extract_dir("Level2", 3))
+
+# Extract covariates -----------------------------------------------------------
+# covariates
+covariates <- c("Age", "Gender", "Education", "Sites", "HeadMotion")
+extract_covars <- extract(covariates)
+
+Covariates <- vector("list", length = 3)
+names(Covariates) <- c("1_12", "2_14", "2_23")
+# Level 1
+Covariates[[1]] <- rbind(extract_covars("Level1", 1), extract_covars("Level1", 2))                            # AONVA
+# Level 2
+Covariates[[2]] <- rbind(extract_covars("Level2", 1), extract_covars("Level2", 4))
+Covariates[[3]] <- rbind(extract_covars("Level2", 2), extract_covars("Level2", 3))
+
+# Fix the Site variable --------------------------------------------------------
+with(Patient, ftable(Level1, Sites))
+with(Patient, ftable(Level1, Level2, Sites))          # 1vs4 needs to be fixed
+
+extract_sites <- extract("Sites")
+Sites <- rbind(extract_sites("Level2", 1), extract_sites("Level2", 4))
+fix_sites <- function(data) {
+  data <- data[[1]]
+  raw <- unique(data)
+  Replace <- seq(raw)
+  for (i in seq(raw)) {
+    data <- replace(data, which(data == raw[i]), Replace[i])
+  }
+  # Abandon useless level
+  data <- droplevels(data)
+  data
+}
+Sites <- fix_sites(Sites)
+Covariates$`2_14`$Sites <- Sites
+
+# Output -----------------------------------------------------------------------
+# Covariates for t-test
+for (i in seq(Covariates)) {
+  write.table(Covariates[[i]], paste0("Covariates", names(Covariates)[[i]], ".txt"), 
+    quote = FALSE, row.names = FALSE, col.names = FALSE)
+}                                                           
+# SPM file directory
+for (i in seq(Dirs)) {
+  write.table(Dirs[[i]], paste0("Dirs", names(Dirs)[[i]], ".txt"), 
+    quote = FALSE, row.names = FALSE, col.names = FALSE)
+}
---
+++ ROI/ROI.R
@@ -0,0 +1,66 @@
+# This script was used to load ROIs signals and regression analysis
+library(xlsx)
+library(dplyr)
+
+
+setwd("E:/WorkingSpace/R/Paper/Depression/ROI")
+# Load data --------------------------------------------------------------------
+home1 <- "E:/WorkingSpace/R/Paper/Depression/HHC/HHC.xlsx"
+# ROIs
+home2 <- "E:/WorkingSpace/R/Paper/Depression/ROI"
+
+patient <- read.xlsx2(home1, 1, stringsAsFactors = FALSE, check.names = FALSE,
+  colClasses = rep(c("character", "numeric"), times = c(8, 29)))
+ROIs <- matrix(nrow = nrow(patient), ncol = 17)
+colnames(ROIs) <- paste0("ROI", seq(ncol(ROIs)))
+
+# Add ROIs mean signals
+for (i in seq(colnames(ROIs))) {
+  data <- read.table(paste0(home2, "ROI", i, ".txt"))
+  data <- data[, , drop = TRUE]
+  ROIs[, i] <- data
+}
+# No NAs
+sum(is.na(ROIs))
+
+# Covariates
+covariates <- c("Age", "Gender", "Education", "Sites", "HeadMotion")
+# PCA components
+pca_vars <- names(patient)[31:34]
+# ROI
+ROIs_vars <- cbind(ROIs, patient[, c(pca_vars, covariates, "Level1")])
+# Convert factor to numeric
+ROIs_vars[] <- lapply(ROIs_vars, as.numeric)
+# Convert somatic and anxiety to SA
+names(ROIs_vars)[20] <- "SA"
+names(ROIs_vars)[21] <- "IL"
+
+# Regression analysis --------------------------------------------------------
+reg <- function(data) {
+  reg_results <- data.frame()
+  for (i in 1:17) {
+    reg <- lm(data[, i] ~ Guilt + Insomnia + SA + IL + Age + Gender + Education + Sites + HeadMotion, 
+      data = data)
+    reg_result <- summary(reg)$coefficients[2:5, 3:4]
+    reg_results  <- rbind(reg_results, reg_result)
+  }
+  
+  reg_tb <- data.frame(rep(names(ROIs_vars)[1:17], each = 4), 
+    rep(names(patient)[31:34], times = 17))
+  reg_df <- cbind(reg_tb, reg_results)
+  names(reg_df) <- c("ROIs", "Components", "Statistic", "P")
+  rownames(reg_df) <- NULL
+  
+  # FDR correction for p-values
+  reg_df$P_corrected <- p.adjust(reg_df$P, method = "fdr")
+  # modify output
+  reg_df$P <- round(reg_df$P, 3)
+  reg_df$P_corrected <- round(reg_df$P_corrected, 3)
+  reg_df$Statistic <- round(reg_df$Statistic, 2)
+  
+  return(reg_df)
+}
+result_all <- reg(ROIs_vars)
+
+# Output -----------------------------------------------------------------------
+write.xlsx2(result_all, "ROI_Regression.xlsx", row.names = FALSE)
