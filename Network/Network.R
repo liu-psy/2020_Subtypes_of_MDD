f80d5e09eae0d2710f2e6de20131eb46cd29e4dd
Yu Liu <liuyupsy@gmail.com>
2020-09-22 14:53:37 +0800

    Update

--- Network/Network.R
+++ Network/Network.R
@@ -73,8 +73,8 @@ predicability2 <- f1(SAI)
 
 # Plot networks; Figure 3A
 colors <- brewer.pal(4, "Pastel1")
-f2 <- function(data, title, pred) {
-  network <- qgraph(data, title = title, pie = pred, layout = L, title.cex = 2.5,
+f2 <- function(net, title, pred) {
+  network <- qgraph(net, title = title, pie = pred, layout = L, title.cex = 2.5,
     maximum = Max, theme = "Hollywood", pieColor = "#FC8D62", border.width = 2, 
     vsize = 9, label.cex = 1, groups = group, color = colors, labels = paste0("X", 1:17)
   )
@@ -136,15 +136,17 @@ ggsave("Figure S6.pdf", width = 14, height = 8)
 
 # Correlation of the centrality between subtypes -------------------------------
 centralities <- c("Betweenness", "Closeness", "ExpectedInfluence", "Strength")
-centra$node <- rep(vars, times = 8)
-centra_list <- centra %>% group_by(graph, measure) %>% 
-  select(graph, measure, node, value) %>% group_split()
+centra$node  <- rep(vars, times = 8)
 
+centra_list  <- centra %>% 
+  select(-type) %>%
+  group_by(graph, measure) %>% 
+  select(graph, measure, node, value) %>% 
+  mutate(rank = rank(value, na.last = FALSE)) %>% 
+  group_split()
 # Add labels of each list
-names(centra_list) <- paste0(
-  rep(c("LOI", "SAI"), each = 4), "_",
-  rep(centralities, times = 2)
-)
+names(centra_list) <- paste0(rep(c("LOI", "SAI"), each = 4), "_", rep(centralities, times = 2))
+centra_list
 
 f4 <- function(centrality){
   var1 <- paste0("LOI_", centrality)
@@ -162,22 +164,24 @@ f4("Closeness")
 f4("ExpectedInfluence")
 f4("Strength")
 
-# Reorder nodes ----------------------------------------------------------------
-decribe_node <- function(group) {
-  # Sort by centrality from large to small
-  lists <- centra_list[[group]][order(centra_list[[group]]$value, decreasing = TRUE), ]
-  return(lists)
+# Mean rank of centralities
+f5 <- function(subtype) {
+  subtype_centralities <- paste0(subtype, "_", centralities)
+  result <- centra_list[[subtype_centralities[1]]]$rank +
+    centra_list[[subtype_centralities[2]]]$rank +
+    centra_list[[subtype_centralities[3]]]$rank +
+    centra_list[[subtype_centralities[4]]]$rank
+  
+  result         <- round(result / length(centralities), 1)
+  names(result)  <- vars
+  # Sorting results
+  ordered_result <- result[order(result, decreasing = TRUE)]
+  result_list    <- list("Origin" = result, "Ordered" = ordered_result)
+  
+  return(result_list)
 }
-rank_nodes <- sapply(names(centra_list), decribe_node, simplify = FALSE)
-
-# Betweenness
-rank_nodes$LOI_Betweenness; rank_nodes$SAI_Betweenness
-# Clossness
-rank_nodes$LOI_Closeness; rank_nodes$SAI_Closeness
-# Expected influence
-rank_nodes$LOI_ExpectedInfluence; rank_nodes$SAI_ExpectedInfluence
-# Strength
-rank_nodes$LOI_Strength; rank_nodes$SAI_Strength
+f5("LOI")
+f5("SAI")
 
 # Network Comparison -----------------------------------------------------------
 # 1 for LOI, 2 for SAI
@@ -187,7 +191,13 @@ q2 <- estimateNetwork(SAI, "EBICglasso", corMethod = "cor_auto")
 # NCT with 10000 iterations
 nct <- NCT(q1, q2, it = 1000, progressbar = TRUE, test.centrality = TRUE,
   centrality = c("closeness", "betweenness", "strength", "expectedInfluence"))
-nct
+summary(nct)
+
+# p-values of local nodes
+nct$diffcen.pval
+# Differences of local nodes; LOI - SAI
+round(nct$diffcen.real, 3)
+
 
 # Plot results of the network structure invariance test
 plot(nct, what = "network")
@@ -207,13 +217,13 @@ kboot2b <- bootnet(q2, nBoots = 1000, nCores = 16, type = "case",
   )
 
 # Plot edge weight CI; Figure S7
-f5 <- function(data, xlab) {
+f6 <- function(data, xlab) {
   plot(data, labels = FALSE, order = "sample") +
     labs(x = xlab) +
     theme(axis.title.x = element_text(face = "bold", size = 15))
 }
-p1 <- f5(kboot1a, "LOI")
-p2 <- f5(kboot2a, "SAI")
+p1 <- f6(kboot1a, "LOI")
+p2 <- f6(kboot2a, "SAI")
 
 p1 + p2 + plot_layout(ncol = 1)
 ggsave("Figure S7.pdf", width = 10, height = 12)
@@ -231,15 +241,15 @@ themes <- theme(
 )
 
 # Plot centrality stability; Figure S8
-f6 <- function(data, xlab) {
+f7 <- function(data, xlab) {
   plot(data, statistics = "all") +
     labs(x = xlab) +
     geom_line(size = 1.5) +
     geom_point(size = 2) +
     themes
 }
-p1 <- f6(kboot1b, "Sampled People (LOI)")
-p2 <- f6(kboot2b, "Sampled People (SAI)")
+p1 <- f7(kboot1b, "Sampled People (LOI)")
+p2 <- f7(kboot2b, "Sampled People (SAI)")
 
 p1 + p2 + plot_layout(ncol = 1)
 ggsave("Figure S8.pdf", width = 12, height = 10)
@@ -249,26 +259,26 @@ cs1 <- corStability(kboot1b)
 cs2 <- corStability(kboot2b)
 
 # Edge weights diff test; Figure S9
-f7 <- function(data, xlab) {
+f8 <- function(data, xlab) {
   plot(data, "edge", plot = "difference", onlyNonZero = TRUE,
     order = "sample", panels = FALSE) +
     labs(x = xlab) +
     theme(axis.title.x = element_text(face = "bold", size = 15))
 }
-p1 <- f7(kboot1a, "LOI")
-p2 <- f7(kboot2a, "SAI")
+p1 <- f8(kboot1a, "LOI")
+p2 <- f8(kboot2a, "SAI")
 
 p1 + p2 + plot_layout(ncol = 1)
 ggsave("Figure S9.pdf", width = 8, height = 12)
 
 # Centrality diff test (Strength); Figure S10
-f8 <- function(data, xlab) {
+f9 <- function(data, xlab) {
   plot(data, "strength", order = "sample", labels = TRUE, panels = FALSE) +
     labs(x = xlab) +
     theme(axis.title.x = element_text(face = "bold", size = 15))
 }
-p1 <- f8(kboot1a, "LOI")
-p2 <- f8(kboot2a, "SAI")
+p1 <- f9(kboot1a, "LOI")
+p2 <- f9(kboot2a, "SAI")
 
 p1 + p2 + plot_layout(ncol = 1)
 ggsave("Figure S10.pdf", width = 8, height = 12)
